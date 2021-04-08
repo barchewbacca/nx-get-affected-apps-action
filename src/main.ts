0f7ed8b445bf93fb7d7aa46647c3ebf5cf3644c0
Ilia Barchugov <barchewbacca@gmail.com>
2021-04-08 10:02:44 +0200

    omit building docker images

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import * as core from '@actions/core';
-import { execSync } from 'child_process';
 import { getAffectedApps } from './getAffectedApps';
 
 export async function run(): Promise<void> {
   try {
     const base = core.getInput('base');
     const head = core.getInput('head');
-    const registry = core.getInput('registry');
-    const version = core.getInput('version');
 
     core.exportVariable('NX_BASE', base || 'HEAD~1');
     core.exportVariable('NX_HEAD', head || 'HEAD');
@@ -19,12 +16,8 @@ export async function run(): Promise<void> {
       head,
     });
 
-    core.setOutput('affected_apps', { apps: affectedApps });
+    core.setOutput('affected_apps', affectedApps);
     core.exportVariable('NX_AFFECTED_APPS', affectedApps);
-
-    for (const app of affectedApps) {
-      execSync(`docker build -t ${registry}/${app}:${version} --build-arg APP=${app} .`);
-    }
   } catch (error) {
     core.setFailed(error.message);
   }
